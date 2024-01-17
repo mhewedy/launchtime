9faf51f4d51724e839d726b26f4c505ba060aa47
mhewedy <mhewedy@elm.sa>
2024-01-17 22:36:21 +0300

    Polish

--- api/route.go
+++ api/route.go
@@ -5,7 +5,8 @@ import "github.com/gin-gonic/gin"
 func Run(addr ...string) error {
 	r := gin.Default()
 
-	r.GET("/proxy", getOriginalResponse)
+	r.GET("/orig", getOriginalResponse)
+
 	r.GET("/categories", getCategories)
 	r.GET("/categories/:id", getCategoryByID)
 	r.GET("/items", searchItems)
